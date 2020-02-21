b310e892c35706abd9e2340b80cfb031245f9acd
hvl580858 <per.h92@gmail.com>
2020-02-21 11:50:41 +0100

    Dat110 - Oblig2
    Ferdig oppgave C
    Lagt til ChApp(oppgave D) Ferdig?

--- src/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ src/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -6,20 +6,30 @@ import no.hvl.dat110.messages.PublishMsg;
 import no.hvl.dat110.common.TODO;
 
 public class DisplayDevice {
-	
+
 	private static final int COUNT = 10;
-		
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		System.out.println("Display starting ...");
-		
+
 		// TODO - START
-				
+		Client displayDevice = new Client("display", Common.BROKERHOST, Common.BROKERPORT);
+		displayDevice.connect();
+		displayDevice.createTopic(Common.TEMPTOPIC);
+		displayDevice.subscribe(Common.TEMPTOPIC);
+		int i = 0;
+		while (i < COUNT) {
+			System.out.println("Displaying: " + displayDevice.receive());
+			i++;
+		}
+
+		displayDevice.disconnect();
 		// TODO - END
-		
+
 		System.out.println("Display stopping ... ");
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+//		throw new UnsupportedOperationException(TODO.method());
+
 	}
 }
--- src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -1,23 +1,29 @@
 package no.hvl.dat110.iotsystem;
 
 import no.hvl.dat110.client.Client;
-import no.hvl.dat110.common.TODO;
 
 public class TemperatureDevice {
-	
+
 	private static final int COUNT = 10;
-	
+
 	public static void main(String[] args) {
-		
+
 		TemperatureSensor sn = new TemperatureSensor();
-		
 		// TODO - start
+		Client sensorClient = new Client("tempSensor", Common.BROKERHOST, Common.BROKERPORT);
+		sensorClient.connect();
+		int i = 0;
+		while (i < COUNT) {
+			sensorClient.publish(Common.TEMPTOPIC, sn.read() + "");
+			i++;
+		}
 		
+		sensorClient.disconnect();
 		// TODO - end
-		
+
 		System.out.println("Temperature device stopping ... ");
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+//		throw new UnsupportedOperationException(TODO.method());
+
 	}
 }
